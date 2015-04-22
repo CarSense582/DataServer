97ee563fde480e2ccad25362127e805cb6235e77
karatekid <mikechri@umich.edu>
2015-04-22 19:09:17 -0400

    couldn't get res in manifest, but added extra data to dataserver

--- app/src/main/java/com/example/michael/dataserver/ContentManagerReceiver.java
+++ app/src/main/java/com/example/michael/dataserver/ContentManagerReceiver.java
@@ -12,6 +12,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ContentManagerReceiver extends BroadcastReceiver {
 
     @Override
@@ -26,7 +29,22 @@ public class ContentManagerReceiver extends BroadcastReceiver {
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
         Bundle results = getResultExtras(true);
+        //Testing send response
         results.putString("broadCastResp","DS responding to CM broadcast");
+        //Testing sending array of ids
+        ArrayList<String> otherServices = results.getStringArrayList("dsServices");
+        if(otherServices == null) {
+            otherServices = new ArrayList<String>();
+        }
+        String serviceId = context.getResources().getString(R.string.ds_cm_id);
+        otherServices.add(serviceId);
+        System.out.println("DS id: " + serviceId);
+        results.putStringArrayList("dsServices", otherServices);
+        //Add service dependent map
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        map.put("a", 1);
+        map.put("b", 5);
+        results.putSerializable(serviceId, map);
     }
 
 }
